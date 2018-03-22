de529b5774d4026683c62d465a3d25e1979e63c3
Jan Meijering <jan@meijering.com>
2018-03-22 07:20:15 +0100

    update configure script

--- scripts/configure.php
+++ scripts/configure.php
@@ -1,12 +1,12 @@
 <?PHP
 
-//define('DB_SERVER', 'ilsevl1.sql.greenhost.nl');
-//define('DB_USER', 'degroenegira');
-//define('DB_PASS', '2orithEt');
+define('DB_SERVER', 'ilsevl1.sql.greenhost.nl');
+define('DB_USER', 'degroenegira');
+define('DB_PASS', '2orithEt');
 
-define('DB_SERVER', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', 'root');
+// define('DB_SERVER', 'localhost');
+// define('DB_USER', 'root');
+// define('DB_PASS', 'root');
 
 define('DB_SALT','cc0ef2357c108509e3aa');
 define('DB_NAME','degroenegiraf_nl_cl2');
